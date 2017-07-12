2ab0fd0f02d29d7a10fcf4ab40839cc55357a043
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-07-12 10:23:01 +0100

    Updating the %'s displayed - in Controller

--- app.js
+++ app.js
@@ -235,10 +235,20 @@ var controller = (function(budgetCtrl, UICtrl){
       var budget = budgetCtrl.getBudget();
 
       // 3. Display the budget on the UI
-      //console.log(budget);
+      // console.log(budget);
       UICtrl.displayBudget(budget);
 
   };
+
+    var updatePercentages = function() {
+        
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+        
+    };
 
     var ctrlAddItem = function() {
        var input, newItem;
@@ -259,8 +269,13 @@ var controller = (function(budgetCtrl, UICtrl){
 
        // 5. Calculate and update budget
        updateBudget();
+
+       // 6. Calculate and update percentages
+       updatePercentages();
        }
     };
+
+    
     
     var ctrlDeleteItem = function(event){
         var itemID, splitID, type, ID;
@@ -282,6 +297,10 @@ var controller = (function(budgetCtrl, UICtrl){
 
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculate and update percentages
+            updatePercentages();
+       
 
         }
     };
